a5ec860d019e62052a9c8e8202764458bd6ce3d1
Sumuyzzz <Sumuyzzz@gmail.com>
2022-11-10 16:36:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" alt="Vite logo" class="p-5 h-24 will-change-filter logo">
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="p-5 h-24 will-change-filter vue">
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
