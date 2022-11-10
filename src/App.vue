1c5fb953a22fdbe354555da8d2554178e2f963b3
Sumuyzzz <Sumuyzzz@gmail.com>
2022-11-11 02:17:38 +0800

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" alt="Vite logo" class="p-5 h-24 will-change-filter logo">
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="p-5 h-24 will-change-filter vue">
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Header />
 </template>
 
 <style scoped>
