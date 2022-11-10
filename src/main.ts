bcb54c9d11e400cbdfeb9556beb9d21c225f74f9
Sumuyzzz <Sumuyzzz@gmail.com>
2022-11-10 17:42:48 +0800

    chore:add element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
 import 'uno.css'
 import 'virtual:unocss-devtools'
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss()],
+  plugins: [vue(), Unocss(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    },
+    ),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   server: {
     port: 3000,
   },
